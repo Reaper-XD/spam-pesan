5abccce0764004b7b00524b2610a1a03d1cf6022
GRCR4K3R <94230332+GRCR4K3R@users.noreply.github.com>
2021-11-19 16:39:11 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -26,7 +26,7 @@ def main():
         jum = input('Jumlah Spam ==> :')
 
         head = {
-        "User-Agent": "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
         "Referer": "https://www.mapclub.com/en/user/signup",
         "Host": "cmsapi.mapclub.com",
         }
